6007aa20fbcb32164e0eebfe621987b14fd6783d
Nozotrox <fifasmazoio@gmail.com>
2018-10-09 15:18:17 +0200

    ::>> Working on Log_In and Sign Up

--- src/Authentication.java
+++ src/Authentication.java
@@ -10,6 +10,26 @@ public class Authentication extends JFrame implements ActionListener {
 
     private Vector<User> users = new Vector<>();
 
+    JLabel username;
+    JLabel password;
+    JLabel nome;
+
+    JTextField username_;
+    JPasswordField password_;
+    JTextField nome_;
+
+    JButton log_in;
+    JButton cancel;
+    JButton cancel2;
+    JButton sign_up;
+
+    JButton confirmar;
+
+    JFrame jf;
+
+    boolean insideReg;
+
+
     public Authentication(){
         build_ui();
     }
@@ -17,7 +37,7 @@ public class Authentication extends JFrame implements ActionListener {
     //::>> UI METHODS
     private void build_ui(){
 
-        this.setPreferredSize(new Dimension(380, 200));
+        this.setPreferredSize(new Dimension(400, 250));
         this.setSize(380,200);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
@@ -25,26 +45,32 @@ public class Authentication extends JFrame implements ActionListener {
         JPanel cred_layout = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 30));
         JPanel flowLayout = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
-        JLabel username = new JLabel("Username");
-        JLabel password = new JLabel("Password");
+        username = new JLabel("Username: ");
+        password = new JLabel("Password: ");
+        nome = new JLabel("     Nome: ");
 
-        JTextField username_ = new JTextField(20);
-        JPasswordField password_ = new JPasswordField(20);
+        username_ = new JTextField(20);
+        password_ = new JPasswordField(20);
+        nome_ = new JTextField(20);
 
-        JButton log_in = new JButton("Log In");
-        JButton cancel = new JButton("Cancelar");
-        JButton sign_up = new JButton("Registrar");
+        log_in = new JButton("Log In");
+        cancel = new JButton("Cancelar");
+        sign_up = new JButton("Registrar");
 
         log_in.addActionListener(this);
         sign_up.addActionListener(this);
         cancel.addActionListener(this);
 
 
+        cred_layout.add(nome);
+        cred_layout.add(nome_);
+
         cred_layout.add(username);
         cred_layout.add(username_);
 
         cred_layout.add(password);
         cred_layout.add(password_);
+
 
         flowLayout.add(log_in);
         flowLayout.add(sign_up);
@@ -57,6 +83,57 @@ public class Authentication extends JFrame implements ActionListener {
         this.add("West", new JLabel("      "));
         this.pack();
         this.setVisible(true);
+
+        nome.setVisible(false);
+        nome_.setVisible(false);
+    }
+
+    private void sign_up_ui(){
+
+        this.insideReg = true;
+
+        jf = new JFrame();
+        jf.setPreferredSize(new Dimension(400, 250));
+        jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+        JPanel cred_layout = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 30));
+        JPanel flowLayout = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        cred_layout.add(nome);
+        cred_layout.add(nome_);
+
+        cred_layout.add(username);
+        cred_layout.add(username_);
+
+        cred_layout.add(password);
+        cred_layout.add(password_);
+
+        confirmar = new JButton("Confirmar");
+        cancel2 = new JButton("Cancelar");
+        cancel2.addActionListener(this);
+
+        confirmar.addActionListener(this);
+
+        flowLayout.add(confirmar);
+        flowLayout.add(cancel2);
+        flowLayout.add(new JLabel("  "));
+
+        jf.add("Center",cred_layout);
+        jf.add("South", flowLayout);
+        jf.add("East", new JLabel("      "));
+        jf.add("West", new JLabel("      "));
+        jf.pack();
+        jf.setVisible(true);
+
+        nome.setVisible(true);
+        nome_.setVisible(true);
+
+    }
+
+    private void log_in_ui(){
+        this.nome.setVisible(false);
+        this.nome_.setVisible(false);
     }
 
     //::>> FUNCTIONALITY METHODS
@@ -172,5 +249,53 @@ public class Authentication extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(e.getSource() == this.sign_up){
+
+            sign_up_ui();
+        }
+
+        if(e.getSource() == this.confirmar || e.getSource() == this.cancel){
+            String nome;
+            String username;
+            String password;
+
+            nome = this.nome_.getText();
+            username = this.username_.getText();
+            password = this.password_.getText();
+
+            if(username.isEmpty()){
+                username = nome;
+            }
+
+            if(UserUtility.isRegistred(nome, username, this.users)){
+                JOptionPane.showMessageDialog(null, "Usuario ja Registrado!");
+
+            }
+            else{
+                User novoUsuario = new User(nome, username, password);
+                this.users.add(novoUsuario);
+                JOptionPane.showMessageDialog(null, "Registrado com Sucesso!");
+
+            }
+
+            if(insideReg){
+                this.username_.setText("");
+                this.password_.setText("");
+                this.nome_.setText("");
+                jf.dispose();
+            }
+
+
+
+            this.insideReg = false;
+        }
+
+        if(e.getSource() == this.cancel){
+            System.exit(0);
+        }
+
+        if(e.getSource() == this.cancel2){
+            this.dispose();
+        }
     }
 }
