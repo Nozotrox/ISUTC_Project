e881ccdfeb78ec4ab2d12371d9cd3e5c03bdbcdd
Kelvin <mabjaiakelvin@gmail.com>
2018-10-15 15:17:29 +0200

    ihjgjhgj

---
+++ src/Screens/MainMenu.java
@@ -0,0 +1,185 @@
+package Screens;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+/**
+ * @author MSI
+ *
+ */
+
+/**
+ *
+ *LEMBRETE URGENTE SENAO BUG LEMBREM-SE DE ADICIONAR UM BACKGROUND
+ *
+ */
+
+public class MainMenu extends JFrame implements ActionListener {
+
+    // MAIN MENU
+    private JButton button1, button2, button3,button4;
+    private JDesktopPane desktopPane;
+    private JMenuBar menuBar;
+    private JMenu item1,item2;
+    private JInternalFrame saleFrame, searchFrame, buyFrame;
+
+    private ImageIcon icon;
+
+    // SALE MENU
+    private JTable selectItem;
+    private JLabel label1;
+
+    // SEARCH MENU
+    private JTable searchItems;
+    private JLabel name, id;
+    private JTextField name_, id_;
+
+    public MainMenu() {
+        build_ui();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        if (arg0.getSource().equals(button1)) {
+            buildProductSale();
+        } else if (arg0.getSource().equals(button2)) {
+            buildProductBuy();
+        } else if (arg0.getSource().equals(button3)) {
+
+        }else if(arg0.getSource().equals(button4)){
+            buildProductSearch();
+        }
+    }
+
+    private void buildProductSale() {
+        saleFrame.setLayout(new BorderLayout());
+        saleFrame.add("Center", new JLabel("VENDA"));
+        saleFrame.setSize(600, 400);
+        saleFrame.setLocation(0,0);
+        saleFrame.toFront();
+        saleFrame.setVisible(true);
+        saleFrame.setDefaultCloseOperation(HIDE_ON_CLOSE);
+    }
+
+    private void buildProductBuy() {
+        buyFrame.setLayout(new BorderLayout());
+        buyFrame.add("Center", new JLabel("Compra"));
+        buyFrame.setSize(600, 400);
+        buyFrame.setLocation(0,0);
+        buyFrame.toFront();
+        buyFrame.setVisible(true);
+        buyFrame.setDefaultCloseOperation(HIDE_ON_CLOSE);
+    }
+
+    private void buildProductSearch() {
+        searchFrame.setLayout(new BorderLayout());
+        searchFrame.add("Center", new JLabel("PESQUISA"));
+        searchFrame.setSize(600, 400);
+        searchFrame.setLocation(0, 0);
+        searchFrame.toFront();
+        searchFrame.setVisible(true);
+        searchFrame.setDefaultCloseOperation(HIDE_ON_CLOSE);
+    }
+
+    private void build_ui() {
+        setLayout(new BorderLayout());
+        setDefaultLookAndFeelDecorated(true);
+
+
+        menuBar=new JMenuBar();
+        item1=new JMenu("Sair");
+        item2=new JMenu("HELP");
+        menuBar.add(item1);
+        menuBar.add(item2);
+        JPanel panel = new JPanel(new GridLayout(4, 1));
+        JPanel panel2 = new JPanel(new BorderLayout());
+
+        panel.setBackground(Color.WHITE);
+
+        icon = new ImageIcon("D:/Kelvin/Documents/Java/ISUTC_Project/src/img/sl.png");
+        button1 = new JButton(icon);
+        button1.setBackground(Color.WHITE);
+        button1.addActionListener(this);
+
+        icon = new ImageIcon("D:/Kelvin/Documents/Java/ISUTC_Project/src/img/buy.png");
+        button2 = new JButton(icon.getImage()+ " COMPRA");
+        button2.setBackground(Color.WHITE);
+        button2.addActionListener(this);
+
+        //icon = new ImageIcon("D:/Kelvin/Documents/Java/ISUTC_Project/src/img/sale.png");
+        button3 = new JButton(" FORNECEDORES");
+        button3.setBackground(Color.WHITE);
+        button3.addActionListener(this);
+
+        icon = new ImageIcon("D:/Kelvin/Documents/Java/ISUTC_Project/src/img/search.png");
+        button4=new JButton(icon.getImage()+ "FILTRO");
+        button4.setBackground(Color.WHITE);
+        button4.addActionListener(this);
+
+
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+
+        saleFrame = new JInternalFrame("Menu de Venda", true, true, true, true);
+        buyFrame = new JInternalFrame("Menu de Compra", true, true, true, true);
+        searchFrame = new JInternalFrame("Menu de Procura", true, true, true, true);
+
+        /**
+         *
+         *LEMBRETE URGENTE SENAO BUG LEMBREM-SE DE ADICIONAR UM BACKGROUND
+         *
+         */
+        desktopPane = new JDesktopPane() {
+            @Override
+            protected void paintComponent(Graphics grphcs) {
+                super.paintComponent(grphcs);
+                //BACKGROUND
+                grphcs.drawImage(new ImageIcon("").getImage(), 0, 0, null);
+            }
+
+            @Override
+            public Dimension getPreferredSize() {
+                //BACKGROUND 2 VEZES
+                return new Dimension(new ImageIcon("").getIconWidth(),
+                        new ImageIcon("").getIconHeight());
+            }
+        };
+
+        desktopPane.setVisible(true);
+        desktopPane.add(saleFrame);
+        desktopPane.add(searchFrame);
+        desktopPane.add(buyFrame);
+
+        icon = new ImageIcon("D:/Kelvin/Documents/Java/ISUTC_Project/src/img/bg.jpg");
+        desktopPane.add(new JLabel(icon));
+
+        panel2.add("North", new JLabel("        "));
+        panel2.add("Center", desktopPane);
+
+        add("North", new JLabel(""));
+        add("Center", panel2);
+        add(BorderLayout.WEST, panel);
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //setUndecorated(true);
+
+        setJMenuBar(menuBar);
+        setSize(1000, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainMenu();
+    }
+
+}
--- src/Screens/Sign_up.java
+++ src/Screens/Sign_up.java
@@ -4,25 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Vector;
 
 import Main.*;
 
 public class Sign_up extends JFrame implements ActionListener {
 
-
-    JLabel username;
-    JLabel password;
-    JLabel nome;
-
     JTextField username_;
     JPasswordField password_;
     JTextField nome_;
 
-    JButton log_in;
     JButton cancel;
     JButton sign_up;
 
+    private ImageIcon icon;
 
     public Sign_up(){
         build_ui();
@@ -30,47 +24,72 @@ public class Sign_up extends JFrame implements ActionListener {
 
     public void build_ui(){
 
-        this.setPreferredSize(new Dimension(400, 250));
-        this.setSize(380,200);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(400, 250));
+        setSize(500,250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+        setUndecorated(true);
+        this.setBackground(Color.white);
 
-        JPanel cred_layout = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 30));
-        JPanel flowLayout = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel pcenter = new JPanel();
+        pcenter.setBackground(Color.white);
+        pcenter.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 70));
+        icon = new ImageIcon("D:/Kelvin/Documents/Java/ISUTC_Project/src/img/addUser.png");
+            JLabel title = new JLabel(icon);
+            title.setFont(new Font("AriaL", Font.BOLD, 25));
+        pcenter.add(title);
 
-        username = new JLabel("Username: ");
-        password = new JLabel("Password: ");
-        nome = new JLabel("     Nome: ");
+        // Left Side
+        JPanel pLeft = new JPanel();
+        pLeft.setBackground(Color.white);
+        pLeft.setLayout(new GridLayout(5,1));
 
-        username_ = new JTextField(20);
-        password_ = new JPasswordField(20);
-        nome_ = new JTextField(20);
+        pLeft.add(new JLabel("Nome: "));
+        pLeft.add(new JLabel(""));
+        pLeft.add(new JLabel("Username: "));
+        pLeft.add(new JLabel(""));
+        pLeft.add(new JLabel("Password: "));
 
-        cancel = new JButton("Cancelar");
-        sign_up = new JButton("Registrar");
+        // Right Side
+        JPanel pRight = new JPanel();
+        pRight.setBackground(Color.white);
+        pRight.setLayout(new GridLayout(5, 1));
+        username_ = new JTextField(12);
+        username_.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
+        password_ = new JPasswordField(12);
+        password_.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
+        nome_ = new JTextField(12);
+        nome_.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
 
+        pRight.add(nome_);
+        pRight.add(new JLabel(""));
+        pRight.add(username_);
+        pRight.add(new JLabel(""));
+        pRight.add(password_);
+
+        JPanel pbtn = new JPanel();
+        pbtn.setBackground(Color.white);
+        pbtn.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        //Button OK
+        sign_up = new JButton("OK");
+        sign_up.setBorder(null);
+        sign_up.setBackground(Color.LIGHT_GRAY);
+        sign_up.setBorder(BorderFactory.createMatteBorder(5, 20, 5, 20, Color.LIGHT_GRAY));
         sign_up.addActionListener(this);
+
+        //Button Cancel
+        cancel = new JButton("Cancel");
+        cancel.setBackground(Color.GRAY);
+        cancel.setBorder(BorderFactory.createMatteBorder(5, 10, 5, 10, Color.GRAY));
         cancel.addActionListener(this);
 
+        pbtn.add(sign_up); pbtn.add(cancel);
 
-        cred_layout.add(nome);
-        cred_layout.add(nome_);
-
-        cred_layout.add(username);
-        cred_layout.add(username_);
-
-        cred_layout.add(password);
-        cred_layout.add(password_);
-
-
-        flowLayout.add(sign_up);
-        flowLayout.add(cancel);
-        flowLayout.add(new JLabel("  "));
-
-        this.add("Center",cred_layout);
-        this.add("South", flowLayout);
-        this.add("East", new JLabel("      "));
-        this.add("West", new JLabel("      "));
+        pcenter.add(pLeft); pcenter.add(pRight);
+        this.add("Center", pcenter);
+        this.add("South", pbtn);
         this.pack();
         this.setVisible(true);
     }
@@ -86,11 +105,13 @@ public class Sign_up extends JFrame implements ActionListener {
         String username;
         String password;
 
+        Authentication au = new Authentication();
+
         if(e.getSource() == cancel){
             this.dispose();
+            au.setVisible(true);
         }
         else if(e.getSource() == sign_up){
-
             nome = this.nome_.getText();
             username = this.username_.getText();
             password = this.password_.getText();
@@ -102,28 +123,20 @@ public class Sign_up extends JFrame implements ActionListener {
             if(!(nome.equals("") || password.equals(""))){
 
                 if(UserUtility.isRegistred(nome, username, UserUtility.users)){
-                    JOptionPane.showMessageDialog(null, "Usuario ja Registrado!");
-
+                    JOptionPane.showMessageDialog(null, "Usuário já registrado!");
+                    au.setVisible(true);
                 }
                 else{
                     User novoUsuario = new User(nome, username, password);
                     UserUtility.users.add(novoUsuario);
-                    JOptionPane.showMessageDialog(null, "Registrado com Sucesso!");
-                    this.username_.setText("");
-                    this.password_.setText("");
-                    this.nome_.setText("");
+                    JOptionPane.showMessageDialog(null, "Usuário "+username+" registrado com sucesso!");
+                    au.setVisible(true);
                     this.dispose();
-
                 }
             }
             else {
-                JOptionPane.showMessageDialog(null, "Por favor, Preencha os campos Obrigatorios: Nome e Password");
+                JOptionPane.showMessageDialog(null, "Por favor, preencha os campos obrigatorios: Nome e Password");
             }
-
-
-
-
-
         }
 
 
