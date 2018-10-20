d408e23a6c9cf2d1d0b46bdc8163167a49bafc06
Nozotrox <fifasmazoio@gmail.com>
2018-10-20 06:50:21 +0200

    :::>> Clock working mthrfckr

--- src/Main_Classes/ClockTimer.java
+++ src/Main_Classes/ClockTimer.java
@@ -19,14 +19,29 @@ public class ClockTimer extends Thread {
         time = jt;
     }
 
+    public String timeFormat(int hour, int minute, int seconds){
+
+        String hour_s = String.valueOf(hour);
+        String minute_s = String.valueOf(minute);
+        String seconds_s = String.valueOf(seconds);
+
+        String f_hour = (hour_s.length() > 1)? hour_s: "0" + hour_s;
+        String f_minute = (minute_s.length() > 1)? minute_s: "0" + minute_s;
+        String f_seconds = (seconds_s.length() > 1)? seconds_s: "0" + seconds_s;
+
+        return f_hour + ":" + f_minute + ":" + f_seconds;
+
+
+    }
+
     public void run(){
 
         try {
             while(true) {
                 this.sleep(1000);
                 now = LocalDateTime.now();
-                time.setText(now.getHour() + ":" + now.getMinute() + ":" + now.getSecond());
-//                System.out.print("running");
+                String timeString = timeFormat(now.getHour(), now.getMinute(), now.getSecond());
+                time.setText(timeString);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
